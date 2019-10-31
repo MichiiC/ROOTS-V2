c56b6684bdc98ad9e9b6e1bb3675bf5f9010580a
Mark Koberlein <mkoberlein@gmail.com>
2019-10-31 14:59:08 -0400

    Added ClueGenerator

---
+++ Assets/Scripts/ClueTextGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ClueTextGenerator : MonoBehaviour
+{
+
+    public GameObject cluePanelText; 
+
+    private ArrayList clueTextList = new ArrayList();
+    private int clueIndex;
+
+    public void GenerateClueText() {
+        Text clueText = cluePanelText.GetComponent<Text>();
+
+        if ( clueIndex < clueTextList.Count) {
+            clueText.text = clueTextList[clueIndex].ToString();
+            clueIndex++;
+        }
+        else {
+            clueIndex = 0;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clueIndex = 0;
+
+        clueTextList.Add("My First Clue");   
+        clueTextList.Add("My Second Clue");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
