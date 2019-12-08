e8217522679da2f0ce52330600e4890f08cb0aa2
MichiiC <32467701+MichiiC@users.noreply.github.com>
2019-12-08 03:00:23 -0500

    Revision 11

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -14,6 +14,8 @@ using UnityEngine.EventSystems;
 
 
 
+
+
 public class PlayerInput : MonoBehaviour
 
 {
@@ -303,7 +305,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToGrass");
 
-                    descriptionText.text = "All Plants have roots that are used to abosrb water and other nutrients from the soil.Unfortunately, the soil has a limited amount of these resourcces, and that means that all the plants in  an area need to compete for these resources. Grass usually wins this fight, because it covers so much more surface area, it chokes the trees out.";
+                    descriptionText.text = "All plants have roots that are used to absorb water and other nutrients from the soil. Unfortunately, the soil has a limited amount of resources, and that means that all the plants in an area are in constant competition with each other. Also, some grasses, such as red fescue and bermudagrass, secrete chemicals that harm the growth of tree roots! (7)";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite grassActiveSprite = Resources.Load<Sprite>("ObjectIcons/Grass_Icon_Active");
@@ -321,7 +323,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToAbandon");
 
-                    descriptionText.text = "Frequently trees are felled to create space to make buildings. It is much better for the trees if we reused the space taken up by abandoned buildings, or just refurbished them (if possible). ";
+                    descriptionText.text = "It’s no secret that deforestation results in billions of trees being cut down each year. If we were to reuse and repurpose old, abandoned lots, we wouldn’t need to kill as many trees to make room for more buildings. (4) ";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite abandonActiveSprite = Resources.Load<Sprite>("ObjectIcons/Abandon_Icon_Active");
@@ -341,7 +343,7 @@ public class PlayerInput : MonoBehaviour
 
                         cameraAnimator.Play("CameraMoveToSmoke");
 
-                        descriptionText.text = "Car or truck emissions effect plant life due to trees and other plants make their own food from carbon dioxide, water, sunlight, and elements found in soil  and then release oxygen for humans and animals to breathe. But exhaust fumes can also affect them by producing carbon monoxide, nitrogen dioxide (contributes to acidic rain), soot, and more.";
+                        descriptionText.text = "The compounds found within vehicle emissions can have a detrimental effect on the plants that are exposed to them. In fact, plants that have to deal with the stress of pollution don’t hold onto water as easily as healthy plants, making them more vulnerable to droughts. (6)";
                         descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                         Sprite smokeActiveSprite = Resources.Load<Sprite>("ObjectIcons/Smoke_Icon_Active");
@@ -359,7 +361,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToLights");
 
-                    descriptionText.text = "The lighting on a construction site is liekly to be powered by generators which work by burning fuel and giving off harmful emissions, which can damage and pollute the local environment. Many insects are also frawn to light, but artificial lights can create a fatal attraction. Declining inspect populations negatively impacts all species that rely on insects for food or pollination.";
+                    descriptionText.text = "The lighting on a construction site is likely to be powered by generators which work by burning fuel and giving off harmful emissions. Many insects are also drawn to light, which can pull insects from their ecosystem, cause them to die of exhaustion, or make them easy prey. Declining insect populations negatively impacts all species that rely on insects for food or pollination. (1) (2) (3)";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite lightsActiveSprite = Resources.Load<Sprite>("ObjectIcons/Lights_Icon_Active");
@@ -377,7 +379,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToTools");
 
-                    descriptionText.text = "Many caution signs are put up where construction or facilities that deal with hazardours or toxic material and disposables. These hazardous and toxic  material affects our environment especially trees which can suck up these harmful toxins and killing them. ";
+                    descriptionText.text = "Depending on the type of construction, hazardous waste can be a real issue. Lead paint, batteries, and toxic chemicals, are only a small portion of what is thrown away at construction sites that have the potential to be dangerous. Especially if they aren’t disposed of properly, these materials can pose a serious threat to the local environment, between tainting the soil and poisoning wildlife. (5)";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite toolActiveSprite = Resources.Load <Sprite>("ObjectIcons/Tool_Icon_Active");
@@ -395,7 +397,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToDog");
 
-                    descriptionText.text = "Certain herbivores dig up or eat seeds before they make it into the ground, preventing tree grouwth before it even begins.";
+                    descriptionText.text = "Certain herbivores dig up or eat seeds before they make it into the ground, preventing tree growth before it even begins. Trees aren’t even safe when fully grown. Some animals, such as squirrels and rabbits, eat the roots and bark of trees, preventing them from growing quickly and making them vulnerable to disease. (8)";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite dogActiveSprite = Resources.Load<Sprite>("ObjectIcons/Dog_Icon_Active");
@@ -413,7 +415,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToTree");
 
-                    descriptionText.text = "Due to the lack of water in the desert, the dry wood is excellent fuel for a fire.";
+                    descriptionText.text = "Due to the lack of water in the desert, the dry wood is excellent fuel for a fire. Careless campers and lightning strikes can cause wildfires to rage in dry climates, and a strong breeze can cause an ecological crisis. (9) (10)";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite treeActiveSprite = Resources.Load<Sprite>("ObjectIcons/Tree_Icon_Active");
@@ -431,7 +433,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToGround");
 
-                    descriptionText.text = "The desert floor is often sandy, rocky, or filled with clay, and in the case of the southwest United States, is very alkaline. These characteristics make it difficult for trees to grow, because most trees we are familiar with require a more acidic soil, and much more water than the desert can provide.";
+                    descriptionText.text = "The desert floor is often sandy, rocky, or filled with clay, and in the case of the southwest United States, very alkaline. These characteristics make it difficult for trees to grow, because most trees more acidic soil, and much more water than the desert can provide. (11)";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite groundActiveSprite = Resources.Load<Sprite>("ObjectIcons/Ground_Icon_Active");
@@ -449,7 +451,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToWell");
 
-                    descriptionText.text = "Heat from the sun casees water to evaporate, dehydrating the trees. This makes it difficult for the trees to create enought food to survive, while also increasing the risk of fires.";
+                    descriptionText.text = "When hot climates have a drought, the upper roots of a tree will go into overdrive, trying to find every last molecule of water. If the soil stays dry, these roots will eventually die out, leaving the tree with a smaller overall root system. When rain eventually returns, this leaves the tree at a disadvantage. With lots of foliage and a lackluster root system, they can’t take full advantage of the water. If this pattern continues, the tree will eventually die. (12)";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite wellActiveSprite = Resources.Load<Sprite>("ObjectIcons/Well_Icon_Active");
@@ -467,7 +469,7 @@ public class PlayerInput : MonoBehaviour
 
                     cameraAnimator.Play("CameraMoveToBird");
 
-                    descriptionText.text = "As animals leave the area populated by trees, the trees can no longer use them to speread their seeds around, making it more difficult to cover more area.";
+                    descriptionText.text = "In an area with a drought, trees aren’t the only ones that need water. Animals will soon leave the area in search of water, and one of two things can happen. Either the pollinating animals migrate away, preventing the trees from spreading its seeds, or the predators in the area leave, allowing the herbivores in the area to run rampant, because they are able to get water from the leaves.";
                     descriptionPanelAnimator.Play("DescriptionPanelSlideOut");
 
                     Sprite birdActiveSprite = Resources.Load<Sprite>("ObjectIcons/Bird_Icon_Active");
---
+++ Assets/Scripts/ToggleVisibility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+
+public class ToggleVisibility : MonoBehaviour
+{
+    
+    public GameObject GameObjectToHide;
+    public float MinTime = 10.0f;
+    public float MaxTime = 20.0f;
+
+    void Start()
+    {
+        StartCoroutine(ToggleVisibilityCo(GameObjectToHide));
+    }
+    void Invoker()
+    {
+        Invoke("appearDisappear", Random.Range(2.0F, 5.0F));
+    }
+    private IEnumerator ToggleVisibilityCo(GameObject someObj)
+    {
+        if (someObj == null) yield break;
+
+        while (true)
+        {
+            someObj.SetActive(!someObj.active);
+
+            yield return new WaitForSeconds(Random.Range(MinTime, MaxTime));
+        }
+
+    }
+    }
